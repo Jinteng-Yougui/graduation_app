a3575b7dfab568a3c4f5c812664f39e08e457d5d
yuki kondo <jinteng.yougui@gmail.com>
2023-08-06 12:15:36 +0900

    created contcts_table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   resources :bookings
   devise_for :users
-  resources :contacts
+  resources :contacts do
+    collection do
+    post :confirm
+    end
   root 'contacts#index'
   resources :categories, only: [:new, :create, :index, :edit, :destroy]
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_06_030147) do
+ActiveRecord::Schema.define(version: 2023_08_06_031448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2023_08_06_030147) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +65,5 @@ ActiveRecord::Schema.define(version: 2023_08_06_030147) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "contacts", "users"
 end
