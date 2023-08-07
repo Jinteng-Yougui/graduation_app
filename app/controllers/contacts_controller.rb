3035087b4d7c7711fcfe59298b65b5bdc7ed3de0
yuki kondo <jinteng.yougui@gmail.com>
2023-08-07 13:41:42 +0900

    missing contacts_edit path

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,17 @@
 class ContactsController < ApplicationController
   def index
+    @contacts = Contact.all
+  end
+
+  def edit
+  end
+
+  def destroy
+  @contact.destroy
+
+    respond_to do |format|
+      format.html { redirect_to contacts_url, notice: "連絡先が削除されました。" }
+      format.json { head :no_content }  
+    end
   end
 end
