13d6605b0f626e514595bd55b6467461d5cf786f
yuki kondo <jinteng.yougui@gmail.com>
2023-08-08 16:24:11 +0900

    added kaminari

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :content, length: {in: 1..140}
+  
+  belongs_to :category
+  belongs_to :contact
 end
