08b19fc2308acc341e0c360fce879861079c8111
yuki kondo <jinteng.yougui@gmail.com>
2023-08-20 16:34:56 +0900

    put some data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |user|
+  User.create!(
+    name: "Dpro#{user+1}",
+    email: "dpro#{user+1}@dpro.com",
+    password: "Dpro#{user+1}"
+  )
+end
+
+5.times do |contact|
+  Contact.create!(
+    name: "WEF#{contact+1}",
+    email: "WEF#{contact+1}@dpro.com",
+  )
+end
+
+category_array = ["誕生日", "クリスマス", "結婚記念日", "新年の挨拶", "その他"]
+start_time_array = ["8/24/2023", "8/30/2023", "12/25/2023", "1/1/2024", "9/1/2023"]
+5.times do |booking|
+  Booking.create!(
+    user_id: 1,
+    title: "Hi",
+    content: "How are you?",
+    contact_id: 1
+    start_time: start_time_array.pop,
+    category: priority_array.pop,
+  )
+end
+  
