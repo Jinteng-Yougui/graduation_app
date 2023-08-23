3b28d9bddf7a31aa8fc0f29ed0ccc3c42a888bbe
yuki kondo <jinteng.yougui@gmail.com>
2023-08-23 23:47:32 +0900

    changed the email of guest users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :bookings, through: :contacts
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(email: 'guest_example@example.com') do |user|
       # user.name = "ゲスト"
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   end
 
   def self.guest_admin
-    find_or_create_by!(email: 'guest_admin@example.com', admin: true) do |user|
+    find_or_create_by!(email: 'guest_admin_example@example.com', admin: true) do |user|
       # user.name = "ゲスト管理者"
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
