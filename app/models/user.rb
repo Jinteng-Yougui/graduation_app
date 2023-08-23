75847e0f9f43d3d9e1ebfad2b96bc96bf72d8de3
yuki kondo <jinteng.yougui@gmail.com>
2023-08-23 21:53:40 +0900

    changed user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
-      user.name = "ゲスト"
+      # user.name = "ゲスト"
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
@@ -16,7 +16,7 @@ class User < ApplicationRecord
 
   def self.guest_admin
     find_or_create_by!(email: 'guest_admin@example.com') do |user|
-      user.name = "ゲスト管理者"
+      # user.name = "ゲスト管理者"
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
